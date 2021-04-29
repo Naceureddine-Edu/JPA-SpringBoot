031cb24996c57a37828162dddd4269f41a43cf37
HANSAOUI_NACEUREDDINE@emsi-edu.ma <HANSAOUI_NACEUREDDINE@emsi-edu.ma>
2021-04-29 17:10:40 +0000

    Ajout Des Prop Age Et Adresse

--- src/main/java/ma/myproject/JPASpringBoot/entities/Patient.java
+++ src/main/java/ma/myproject/JPASpringBoot/entities/Patient.java
@@ -22,7 +22,7 @@ public class Patient {
 	private Date dateNaissance;
 	private int score;
 	private boolean malade;
-	
-	// Ceci est un commentaire
+	private int age;
+	private String adresse;
 
 }
