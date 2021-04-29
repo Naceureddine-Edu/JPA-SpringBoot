ecbdd5d302c18bbb5fdbef54be2dd199fc78c8b3
HANSAOUI_NACEUREDDINE@emsi-edu.ma <HANSAOUI_NACEUREDDINE@emsi-edu.ma>
2021-04-29 16:35:21 +0000

    Ajout De La Methode findByNameAndScore

--- src/main/java/ma/myproject/JPASpringBoot/repositories/PatientRepository.java
+++ src/main/java/ma/myproject/JPASpringBoot/repositories/PatientRepository.java
@@ -1,9 +1,12 @@
 package ma.myproject.JPASpringBoot.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ma.myproject.JPASpringBoot.entities.Patient;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
+	public List<Patient> findByNameAndScore(String name, int score);
 }
