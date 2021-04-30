06c3c1a2bdededa611c1c0f11652d83e62209158
HANSAOUI_NACEUREDDINE@emsi-edu.ma <HANSAOUI_NACEUREDDINE@emsi-edu.ma>
2021-04-30 13:35:22 +0000

    Implementation Des Methode CRUD Avec La Base De Donnees H2

--- src/main/java/ma/myproject/JPASpringBoot/repositories/PatientRepository.java
+++ src/main/java/ma/myproject/JPASpringBoot/repositories/PatientRepository.java
@@ -1,12 +1,24 @@
 package ma.myproject.JPASpringBoot.repositories;
 
+
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ma.myproject.JPASpringBoot.entities.Patient;
 
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
-	public List<Patient> findByNameAndScore(String name, int score);
+	public List<Patient> findByScoreGreaterThan(int score);
+	
+	//public List<Patient> findByNameContainsIgnoreCase(String name);
+	
+	public Page<Patient> findByNameContainsIgnoreCase(String name, Pageable pageable);
+	
+	public List<Patient> findByMalade(boolean malade);
+	
+	//public List<Patient> findbyNameEndingWith(String name);
 }
