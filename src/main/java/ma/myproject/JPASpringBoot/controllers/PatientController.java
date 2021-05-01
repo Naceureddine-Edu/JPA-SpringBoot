78029dbc713fefb45d8774c5155819e7ebc977da
HANSAOUI_NACEUREDDINE@emsi-edu.ma <HANSAOUI_NACEUREDDINE@emsi-edu.ma>
2021-05-01 12:05:38 +0000

    Ajout De La Pagination

--- src/main/java/ma/myproject/JPASpringBoot/controllers/PatientController.java
+++ src/main/java/ma/myproject/JPASpringBoot/controllers/PatientController.java
@@ -3,9 +3,12 @@ package ma.myproject.JPASpringBoot.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ma.myproject.JPASpringBoot.entities.Patient;
 import ma.myproject.JPASpringBoot.repositories.PatientRepository;
@@ -24,10 +27,14 @@ public class PatientController {
 	}
 	
 	@GetMapping(path = "/patients")
-	public String listPatient(Model model)
+	public String listPatient(Model model,
+			@RequestParam(name = "page", defaultValue = "0")int page,
+			@RequestParam(name = "size", defaultValue = "7")int size )
 	{
-		List<Patient> patient = patientrepository.findAll();
-		model.addAttribute("listePatients",patient);
+		Page<Patient> pagePatient = patientrepository.findAll(PageRequest.of(page, size));
+		model.addAttribute("listePatients",pagePatient.getContent());
+		model.addAttribute("pages", new int[pagePatient.getTotalPages()]);
+		model.addAttribute("currentPage", page);
 		return "patients";
 	}
 
